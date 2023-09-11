b37c320b83cea4a53cc1725ce02f96693e5bad74
hmy <hmy5004@naver.com>
2023-09-11 20:58:50 +0900

    0908

---
+++ debug.py
@@ -0,0 +1,20 @@
+'''전체주석 : 
+여러줄 주석 '''
+
+"""전체주석"""
+
+i=1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+
+def myfunction():
+    a= 1+2
+    print(a)
+    
+myfunction()
+    
+print('프로그램 종료') 
+    
+    
---
+++ hello.py
@@ -0,0 +1,9 @@
+print("1. Hello Python!")
+print("2. Hello Python!")
+print("3. Hello Python!")
+print("4. Hello Python!")
+print("5. Hello Python!")
+# 주석 ctrl + / 한 줄 주석, 해제시 한번 더 실행
+# 글자 크기 조절 : ctrl + ,- / ctrl += : 전체 메뉴가 커짐 / ctrl + 0 기본값으로 바뀜
+# Break Point : 분석하고자 하는 라인 클릭하여 설정. 설정한 포인트 전에 멈춤.
+
